90a2a5add869912b469d70ce5ff7441b46175d7d
Max Berliński <maxber.gdynia@gmail.com>
2023-02-05 21:34:08 +0100

    removed unwanted files

--- triangle/target/debug/deps/triangle-c31934f7cd7043ae.d
+++
@@ -1,5 +0,0 @@
-/home/max/Desktop/python/ITN/triangle/target/debug/deps/triangle-c31934f7cd7043ae.rmeta: src/main.rs
-
-/home/max/Desktop/python/ITN/triangle/target/debug/deps/triangle-c31934f7cd7043ae.d: src/main.rs
-
-src/main.rs:
